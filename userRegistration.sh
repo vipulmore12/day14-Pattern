4420f27f335a8b445895bba80dab80309af37381
vipulmore12 <vipulmore1998@gmail.com>
2020-12-18 23:14:11 +0530

    Last name pattern

--- userRegistration.sh
+++ userRegistration.sh
@@ -8,3 +8,12 @@ then
 else
 	echo "Invalid First Name"
 fi
+echo "Enter Last Name"
+read lname
+patLname="^[A-Z]{1}[a-zA-Z]*$"
+if [[ $lname =~ $patLname ]]
+then
+	echo "Valid Last Name"
+else
+	echo "Invalid Last Name"
+fi
